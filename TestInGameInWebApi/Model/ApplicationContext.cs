68da5a20d5035e36a0027d7059f9ed57151fc993
Сентемов Василий <sentemovv55@gmail.com>
2023-02-15 21:08:43 +0400

    добавили EF

---
+++ TestInGameInWebApi/Model/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestInGameInWebApi.Model
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
+
--- TestInGameInWebApi/Program.cs
+++ TestInGameInWebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using TestInGameInWebApi.Model;
 
 List<Author> authors= new() 
@@ -16,10 +17,13 @@ List<Author> authors= new()
     }
 };
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 
 // Add services to the container.
 
@@ -32,26 +36,55 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/weatherforecast", () => authors.ToList());
-app.MapGet("/weatherforecast/{id}", (int id) => authors.FirstOrDefault(x => x.AuthorId == id));
+app.MapGet("/weatherforecast", (ApplicationContext db) => db.Authors.ToList());
+app.MapGet("/weatherforecast/{id}", (ApplicationContext db, int id ) => db.Authors.FirstOrDefault(x => x.AuthorId == id));
+//app.MapGet("/weatherforecast", () => authors.ToList());
+//app.MapGet("/weatherforecast/{id}", (int id) => authors.FirstOrDefault(x => x.AuthorId == id));
 
-app.MapPost("/weatherforecast", (Author author) => authors.Add(author));
-
-app.MapPut("/weatherforecast", (Author author) => 
-{ 
-    var s = authors.FindIndex(x =>x.AuthorId == author.AuthorId);
-    if(s < 0) return;
-    authors[s].AuthorId = author.AuthorId;
-    authors[s].Name = author.Name;
-    authors[s].Birthday= author.Birthday;
+//app.MapPost("/weatherforecast", (Author ) => authors.Add(author));
+app.MapPost("/weatherforecast", (Author author, ApplicationContext db) =>
+{
+    db.Authors.Add(author);
+    db.SaveChanges();
+    return author;
 });
 
-app.MapDelete("/weatherforecas/{id}", (int id) => 
+
+//app.MapPut("/weatherforecast", (Author author) => 
+//{ 
+//    var s = authors.FindIndex(x =>x.AuthorId == author.AuthorId);
+//    if(s < 0) return;
+//    authors[s].AuthorId = author.AuthorId;
+//    authors[s].Name = author.Name;
+//    authors[s].Birthday= author.Birthday;
+//});
+
+app.MapPut("/weatherforecast" , (ApplicationContext db, Author author) =>
 {
-    var VremenPerem = authors.FindIndex(x => x.AuthorId == id);
-    if (VremenPerem < 0) return;
-    authors.RemoveAt(VremenPerem);
-    
+    var s = db.Authors.FirstOrDefault(x => x.AuthorId == author.AuthorId);
+    if (s is not null)
+    {
+        s.AuthorId = author.AuthorId;
+        s.Name = author.Name;
+        s.Birthday = author.Birthday;
+        db.Authors.Update(s);
+        db.SaveChanges();
+    }
+});
+
+//app.MapDelete("/weatherforecas/{id}", (int id) => 
+//{
+//    var VremenPerem = authors.FindIndex(x => x.AuthorId == id);
+//    if (VremenPerem < 0) return;
+//    authors.RemoveAt(VremenPerem);
+//});
+
+app.MapDelete("/weatherforecas/{id}", (int id, ApplicationContext db) =>
+{
+    var VremenPerem = db.Authors.FirstOrDefault(x => x.AuthorId == id);
+    if (VremenPerem is null) return;
+    db.Authors.Remove(VremenPerem);
+    db.SaveChanges();
 });
 
 
